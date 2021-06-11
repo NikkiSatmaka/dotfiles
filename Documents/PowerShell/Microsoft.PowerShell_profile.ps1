1d662f7ee2b66ae073b8d60d06a168b899302a8a
Nikki Satmaka <nsatmaka@gmail.com>
2021-06-11 08:44:19 +0700

    add open source powershell settings

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Starship init
+Invoke-Expression (&starship init powershell)
