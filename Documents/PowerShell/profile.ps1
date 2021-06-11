8e610d0f35ff56170562e9569c8cd748fa702bce
Nikki Satmaka <nsatmaka@gmail.com>
2021-06-11 15:17:58 +0700

    modify powershell config

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,4 +1,13 @@
 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Starship init
+Invoke-Expression (&starship init powershell)
+
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 (& "C:\tools\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
--- Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# Starship init
-Invoke-Expression (&starship init powershell)
