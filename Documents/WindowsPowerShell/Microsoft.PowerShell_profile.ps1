e09f9c85f1ccae8c301b0c47f86b90f14984c928
Nikki Satmaka <nsatmaka@gmail.com>
2021-06-25 17:33:59 +0700

    modify powershell profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,12 +6,9 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 # Starship init
-Invoke-Expression (&starship init powershell)
-
-# #region conda initialize
-# # !! Contents within this block are managed by 'conda init' !!
-# (& "C:\tools\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-# #endregion
+if (Get-Command starship -errorAction SilentlyContinue) {
+  Invoke-Expression (&starship init powershell)
+}
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
