cb7af4b860b5f5f802496c83085e99c0c780c1ff
Nikki Satmaka <nsatmaka@gmail.com>
2021-06-25 15:44:32 +0700

    modify powershell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Starship init
+Invoke-Expression (&starship init powershell)
+
+# #region conda initialize
+# # !! Contents within this block are managed by 'conda init' !!
+# (& "C:\tools\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+# #endregion
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+if (Get-Command conda -errorAction SilentlyContinue) {
+  (& "C:\tools\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+}
+#endregion
+
+
