49252bf491e40d473c6d808db8a0e050dbfd1e65
Nikki Satmaka <nsatmaka@gmail.com>
2024-05-08 12:03:38 +0700

    update powershell config

--- private_Documents/private_WindowsPowerShell/private_Microsoft.PowerShell_profile.ps1
+++ private_Documents/private_WindowsPowerShell/private_Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,20 @@
 
 $ENV:STARSHIP_CONFIG = "$HOME\config\starship\config.toml"
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+# # Chocolatey profile
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#   Import-Module "$ChocolateyProfile"
+# }
 
 # Starship init
 if (Get-Command starship -errorAction SilentlyContinue) {
   Invoke-Expression (&starship init powershell)
 }
 
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-if (Get-Command conda -errorAction SilentlyContinue) {
-  (& "$HOME\mambaforge\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-}
-#endregion
-
-
+# #region conda initialize
+# # !! Contents within this block are managed by 'conda init' !!
+# if (Get-Command conda -errorAction SilentlyContinue) {
+#   (& "$HOME\mambaforge\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+# }
+# #endregion
