3901c09a925eec7d80bff6080686bc313859f615
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-02-12 20:57:32 +0700

    add yazi function in fish

--- home/private_dot_config/private_fish/private_config.fish
+++ home/private_dot_config/private_fish/private_config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 
 # Enable linuxbrew completion.
-if command -v brew > /dev/null 2>&1
+if command -q brew
     if test -d (brew --prefix)"/share/fish/completions"
         set -p fish_complete_path (brew --prefix)/share/fish/completions
     end
@@ -13,6 +13,17 @@ if command -v brew > /dev/null 2>&1
 end
 
 # Enable starship prompt.
-if command -v starship >/dev/null 2>&1
+if command -q starship
     starship init fish | source
+end
+
+if command -q yazi
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
 end
