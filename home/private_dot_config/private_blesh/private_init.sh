5bcb6193170be2b17b0b8b149267919930006e82
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-10-24 23:37:46 +0700

    add ble.sh config

---
+++ home/private_dot_config/private_blesh/private_init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+bleopt default_keymap=vi
+
+ble-bind -m vi_nmap --cursor 2
+ble-bind -m vi_imap --cursor 5
+ble-bind -m vi_omap --cursor 4
+ble-bind -m vi_xmap --cursor 2
+ble-bind -m vi_cmap --cursor 0
