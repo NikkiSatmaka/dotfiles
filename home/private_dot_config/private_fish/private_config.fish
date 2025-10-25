f0853c038bd0904821614515e4b610500640571e
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-10-25 09:22:52 +0700

    update fish config

--- home/private_dot_config/private_fish/private_config.fish
+++ home/private_dot_config/private_fish/private_config.fish
@@ -1,24 +1,3 @@
-# Define a function to conditionally initialize shell tools
-function _initialize_fish_tool
-    set -l tool_name $argv[1]
-    set -l init_command $argv[2]
-    set -l init_type $argv[3] # "source" or "eval"
-
-    if command -q $tool_name
-        switch $init_type
-            case "source"
-                # Use string expansion directly for source, as 'eval' isn't needed
-                # and can sometimes cause issues with output redirection.
-                source $init_command
-            case "eval"
-                eval $init_command
-            case "*"
-                # Default to source if not specified or unknown
-                source $init_command
-        end
-    end
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -33,10 +12,6 @@ if command -q brew
         set -p fish_complete_path "$brew_prefix/share/fish/vendor_completions.d"
     end
 end
-
-# _initialize_fish_tool "fzf" "(fzf --fish)" "source"
-# _initialize_fish_tool "starship" "(starship init fish)" "source"
-# _initialize_fish_tool "direnv" "(direnv hook fish)" "source"
 
 fzf --fish | source
 starship init fish | source
