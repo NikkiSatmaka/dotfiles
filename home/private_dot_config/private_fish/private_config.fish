52580eb1fe75972881d637c62544ceb8330609a6
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-10-25 09:40:14 +0700

    update fish config

--- home/private_dot_config/private_fish/private_config.fish
+++ home/private_dot_config/private_fish/private_config.fish
@@ -1,33 +1,20 @@
+#!/usr/bin/env fish
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
 end
 
-# Enable Linuxbrew completion.
-if command -q brew
-    set -l brew_prefix (brew --prefix)
-    if test -d "$brew_prefix/share/fish/completions"
-        set -p fish_complete_path "$brew_prefix/share/fish/completions"
-    end
-    if test -d "$brew_prefix/share/fish/vendor_completions.d"
-        set -p fish_complete_path "$brew_prefix/share/fish/vendor_completions.d"
-    end
-end
+fish_vi_key_bindings
 
 fzf --fish | source
-starship init fish | source
 direnv hook fish | source
 zoxide init fish | source
 
-if command -q yazi
-    function y
-        set tmp (mktemp -t "yazi-cwd.XXXXXX")
-        yazi $argv --cwd-file="$tmp"
-        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            builtin cd -- "$cwd"
-        end
-        rm -f -- "$tmp"
-    end
+function y
+  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+  yazi $argv --cwd-file="$tmp"
+  if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    builtin cd -- "$cwd"
+  end
+  rm -f -- "$tmp"
 end
-
-# Clean up the temporary function
-functions --erase _initialize_fish_tool
