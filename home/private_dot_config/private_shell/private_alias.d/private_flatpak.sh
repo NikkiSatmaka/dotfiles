17369c69536991248052ac2ccbfc5349d894593b
Nikki Satmaka <nsatmaka@gmail.com>
2026-01-08 13:28:01 +0700

    add alias for flatpak apps

---
+++ home/private_dot_config/private_shell/private_alias.d/private_flatpak.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# shellcheck shell=sh
+
+alias protontricks='flatpak run com.github.Matoking.protontricks'
+alias protontricks-launch='flatpak run --command=protontricks-launch com.github.Matoking.protontricks'
