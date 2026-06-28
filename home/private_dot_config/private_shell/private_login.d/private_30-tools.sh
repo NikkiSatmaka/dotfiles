5b7811de8a29172a69407fe203f23c79f432ecad
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-06-28 11:13:15 +0700

    add bun env var to login env

--- home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
+++ home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
@@ -5,3 +5,4 @@ export ZVM_SYSTEM_CLIPBOARD_ENABLED=true
 export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --border top'
 export EDITOR=nvim
 export PIP_REQUIRE_VIRTUALENV=true
+export BUN_INSTALL="${XDG_DATA_HOME:-$HOME/.local/share}/bun"
