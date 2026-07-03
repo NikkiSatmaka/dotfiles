4fcd563b1d137b31d2a951065edee2c740f82e1d
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-07-04 00:02:39 +0700

    update paths

--- home/private_dot_config/private_shell/private_login.d/private_10-paths.sh
+++ home/private_dot_config/private_shell/private_login.d/private_10-paths.sh
@@ -5,5 +5,4 @@ init_brew
 path_prepend "$HOME/.local/bin"
 # path_prepend "$HOME/.local/sbin"
 # path_prepend "$HOME/.emacs.d/bin"
-path_prepend "$(brew --prefix rustup)/bin"
 # path_prepend "${XDG_CONFIG_HOME:-$HOME/.config}/emacs/bin"
