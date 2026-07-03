9534830b955b89eb1723bfd93d4d4a30a5286a49
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-07-03 15:06:29 +0700

    add pnpm and bun to path

--- home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
+++ home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
@@ -6,3 +6,4 @@ export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --borde
 export EDITOR=nvim
 export PIP_REQUIRE_VIRTUALENV=true
 export BUN_INSTALL="${XDG_DATA_HOME:-$HOME/.local/share}/bun"
+export PNPM_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/pnpm"
