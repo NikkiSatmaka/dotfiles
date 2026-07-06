34c56592e704c972f38b3f113bcd707b5e1785d3
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-07-06 13:06:37 +0700

    add function for sesh

--- home/private_dot_config/private_shell/private_functions.d/private_utils.sh
+++ home/private_dot_config/private_shell/private_functions.d/private_utils.sh
@@ -49,3 +49,20 @@ n() {
     rm -f -- "$NNN_TMPFILE" >/dev/null
   }
 }
+
+sesh-connect() {
+  sesh connect "$(
+    sesh list --icons | fzf-tmux -p 80%,70% \
+      --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+      --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+      --bind 'tab:down,btab:up' \
+      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+      --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+      --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+      --preview-window 'right:55%' \
+      --preview 'sesh preview {}'
+  )"
+}
