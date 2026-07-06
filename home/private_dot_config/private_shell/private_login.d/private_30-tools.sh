79ff4bca1031e49a39ffa357a532f17b0ffbb09d
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-07-06 13:06:11 +0700

    update fzf env var

--- home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
+++ home/private_dot_config/private_shell/private_login.d/private_30-tools.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 
 export ZVM_SYSTEM_CLIPBOARD_ENABLED=true
-export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --border top'
+export FZF_DEFAULT_OPTS='--height 40% --popup bottom,40% --layout reverse --border top'
 export EDITOR=nvim
 export PIP_REQUIRE_VIRTUALENV=true
 export BUN_INSTALL="${XDG_DATA_HOME:-$HOME/.local/share}/bun"
