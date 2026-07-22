671756a056d5c1700a9df6345bc03fb3f1907804
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2026-07-23 00:55:35 +0700

    update zsh config: use antidote bundle

--- home/private_dot_config/private_shell/private_interactive.zsh
+++ home/private_dot_config/private_shell/private_interactive.zsh
@@ -16,9 +16,23 @@ fi
 
 ssource "${ZDOTDIR:-$HOME/.config/zsh}/.zstyles"
 
-# source antidote
-source "$(brew --prefix antidote)/share/antidote/antidote.zsh"
-antidote load
+# Set the root name of the plugins files (.txt and .zsh) antidote will use.
+zsh_plugins="${ZDOTDIR:-$HOME/.config/zsh}/.zsh_plugins"
+
+# Ensure the .zsh_plugins.txt file exists so you can add plugins.
+[[ -f ${zsh_plugins}.txt ]] || touch ${zsh_plugins}.txt
+
+# Lazy-load antidote from its functions directory.
+fpath=("$(brew --prefix antidote)/share/antidote/functions" $fpath)
+autoload -Uz antidote
+
+# Generate a new static file whenever .zsh_plugins.txt is updated.
+if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+  antidote bundle <${zsh_plugins}.txt >|${zsh_plugins}.zsh
+fi
+
+# Source your static plugins file.
+source ${zsh_plugins}.zsh
 
 # Hotfix for `dtop` alias from omz:docker conflicting with `dtop` cli tools
 alias dtop >/dev/null 2>&1 && unalias dtop
